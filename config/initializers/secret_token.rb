50f68c22c8d9774c66e36026e24e8da79d37d8e8
Billie Alice Thompson <billie@billiecodes.com>
2013-05-06 15:45:11 +0100

    Setup basic project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrabble::Application.config.secret_token = 'b9ec5395d0896df41994934951bc9177adf4ca618ff3f39b106bed15312470ad84fd277cd13de73a2efd797d58da25c53ec1dd6e9ad49ddd82c23f54991852a7'
