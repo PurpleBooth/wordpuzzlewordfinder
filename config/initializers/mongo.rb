fe2a3f31e727feab7d8477cee6a27841761c3d95
Billie Alice Thompson <billie@billiecodes.com>
2013-05-07 15:40:02 +0100

    Hopefully fixed the db problem

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,8 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#myapp-#{Rails.env}"
+config_file = Rails.root.join('config/mongo.yml')
+if config_file.file?
+  config = YAML.load(ERB.new(config_file.read).result)
+  MongoMapper.setup(config, Rails.env, :logger => Rails.logger)
+end
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
