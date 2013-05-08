930caf108ee5374d4bcc4eac937e732e1d27d610
Billie Alice Thompson <billie@billiecodes.com>
2013-05-08 09:15:50 +0100

    Added what you've searched for at the top of the search results page

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -3,20 +3,19 @@ class DictionaryController < ApplicationController
   def search
     expires_in 24.hours, :public => true
   
+    @tiles = ""
+    @tiles = params[:tiles] if params.key? :tiles
+    @mask = ""
+    @mask = params[:mask] if params.key? :mask
+
     unless params.key?(:tiles) || params.key?(:mask)
       render :status => 400
       return
     end
 
-    @tiles = params[:tiles] if params.key? :tiles
-    @tiles = ""
-    @mask = ""
-
     if params.key?(:mask) && !params[:mask].empty? && params.key?(:tiles) && !params[:tiles].empty?
-      @mask = params[:mask]
       @words = DictionaryWord.find_scrabble_words(params[:tiles], params[:mask])
     elsif params.key?(:mask) && !params[:mask].empty? && (!params.key?(:tiles) || params[:tiles].empty?)
-      @mask = params[:mask]
       @words = DictionaryWord.find_scrabble_words(false, params[:mask])
     else
       @words = DictionaryWord.find_scrabble_words params[:tiles] 
@@ -24,7 +23,6 @@ class DictionaryController < ApplicationController
 
     if @words.empty?
       render :status => 404
-      return
     end
 
   end
