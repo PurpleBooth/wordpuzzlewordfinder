09220ef91a67fb2b3e8488f97bd04b25ca71ec0e
Billie Alice Thompson <billie@billiecodes.com>
2013-05-20 05:50:14 +0100

    Removed experiment code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,6 @@
 class HomeController < ApplicationController
  
   def index
-    @original = true
     expires_in 24.hours, :public => true
-  end
-  
-  def no_consonants
-    expires_in 24.hours, :public => true
-    render "index", :layout => 'no_consonants'
-  end
-  
-  def blurb_text_missing
-    expires_in 24.hours, :public => true
-  end
-  
-  def no_consonants_no_blurb
-    expires_in 24.hours, :public => true
-    render "blurb_text_missing", :layout => 'no_consonants'
   end
 end
