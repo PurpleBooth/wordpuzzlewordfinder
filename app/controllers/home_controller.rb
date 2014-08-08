08b01c9b8644429b211be371d470e1f66e38360f
Billie Alice Thompson <billie@billiecodes.com>
2014-08-08 02:08:50 +0100

    Removed none https amazon associates code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,4 @@ class HomeController < ApplicationController
   def index
     expires_in 24.hours, :public => true
   end
-  
-  def ws
-    expires_in 24.hours, :public => true
-  end
-  
-  def wd
-    expires_in 24.hours, :public => true
-  end
-  
-  def wdws
-    expires_in 24.hours, :public => true
-  end
 end
